4f4b6e61911e39d3473d222c024eceac8ca7d4a7
Alaa El Helw <work@sunny.fr>
2017-02-27 16:00:46 +0100

    Updating demo styles

--- demo/src/portfolio/components/PorfolioComponent.js
+++ demo/src/portfolio/components/PorfolioComponent.js
@@ -3,24 +3,24 @@ import config from '../config.json'
 import {connect} from 'react-redux'
 import {Link} from 'react-router'
 import {AbstractCollectionComponent, mixerConnector} from '../../../../lib'
+import Loader from '../containers/Loader'
 
-const shorten = (str) => str.length > 12 ? str.substr(0,12) + '...' : str
+const shorten = (str) => str.length > 25 ? str.substr(0,25) + '...' : str
 
 class PorfolioItem extends Component {
     render() {
         const {item} = this.props
-        return !item.images[0].path ? null : ( <div className="col-sm-4 col-md-3">
-                <div className="thumbnail">
-                    <img src={item.images[0].path} alt={item.title} style={{}}/>
+        return !item.images[0].path ? null : (
+                <div className="thumbnail paper animated fadeIn">
+                    <Link to={'/' + item.id}><img src={item.images[0].path} alt={item.title} style={{}}/></Link>
                     <div className="caption">
                         <h3>{shorten(item.title)}</h3>
-                        <p><Link to={'/' + item.id}>see more</Link> </p>
                         <p>
                             {item.tags.slice(0,3).map(tag=>(<span style={{marginRight:3}} key={tag.id} className="label label-primary">{tag.title}</span>))}
                         </p>
                     </div>
                 </div>
-            </div>)
+            )
     }
 }
 class PorfolioComponent extends AbstractCollectionComponent {
@@ -33,9 +33,9 @@ class PorfolioComponent extends AbstractCollectionComponent {
             console.log(this.getMetas('error'))
             return (<div className="alert alert-danger" role="alert">...mmm, something wrong happened...</div>)
         }
-        if (!this.isLoaded()) return (<div>loading....</div>)
+        if (!this.isLoaded()) return (<Loader />)
         const items = this.getCollection().map(item => <PorfolioItem key={item.id} item={item}/>)
-        return (<div className="row">
+        return (<div>
             {items}
         </div>)
     }
