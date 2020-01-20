5f39df9ce836566feba18752f5e6905c08e94203
Alaa El Helw <sicksadsun@gmail.com>
2020-01-20 17:53:02 +0100

    Adding Deprecated names components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
+
+
+
 export {AbstractContainer} from './containers/AbstractContainer'
 export {default as AbstractCollectionContainer} from './containers/AbstractCollectionContainer'
 export {default as AbstractModelContainer} from './containers/AbstractModelContainer'
 export {default as AbstractFormModelContainer} from './containers/AbstractFormModelContainer'
+
+//MEANT TO BE REMOVED
+// FS OF LEGACY IMPL
+export {default as AbstractComponent} from './containers/AbstractContainer'
+export {default as AbstractCollectionComponent} from './containers/AbstractCollectionContainer'
+export {default as AbstractModelComponent} from './containers/AbstractModelContainer'
+export {default as AbstractFormModelComponent} from './containers/AbstractFormModelContainer'
+
 export {default as ReduxActionsList} from './actions/ReduxActionsList'
 export {default as mixerStore} from './reducers/mixerStore'
 export {default as mixerConnector} from './mixerConnector'
