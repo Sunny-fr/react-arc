34c38aba71d7aa4f9ae9149c0c724137f79f949e
Alaa El Helw <sicksadsun@gmail.com>
2021-01-22 18:14:45 +0100

    Upgrading Demo

--- demo/src/portfolio/components/PorfolioEditItemComponent.js
+++ demo/src/portfolio/components/PorfolioEditItemComponent.js
@@ -1,82 +1,105 @@
-import React  from 'react'
-import config from '../config.json'
-import {connect} from 'react-redux'
+import React from "react"
+import config from "../config.json"
+import { connect } from "react-redux"
 //import {Link, hashHistory} from 'react-router'
-import {AbstractFormModelContainer, mixerConnector} from '../../lib'
-import {Loader} from '../../layout/components/loader'
-import {Toolbar} from '../../layout/components/toolbar'
-import {Toast} from '../../layout/components/toast'
-import {Error} from '../../layout/components/error'
-import Link from '../../navigation/Link'
-import {navigateTo} from '../../navigation/navigate'
-
+import { AbstractFormModelContainer, mixerConnector } from "../../lib"
+import { Loader } from "../../layout/components/loader"
+import { Toolbar } from "../../layout/components/toolbar"
+import { Toast } from "../../layout/components/toast"
+import { Error } from "../../layout/components/error"
+import Link from "../../navigation/Link"
+import { navigateTo } from "../../navigation/navigate"
 
 function FormRow(props) {
-    const {name, children} = props
-    return (<div className="form-group">
-        <label htmlFor={'input-' + name} className="col-sm-2 control-label">{name}</label>
-        <div className="col-sm-10">{children}</div>
-    </div>)
+  const { name, children } = props
+  return (
+    <div className="form-group">
+      <label htmlFor={"input-" + name} className="col-sm-2 control-label">
+        {name}
+      </label>
+      <div className="col-sm-10">{children}</div>
+    </div>
+  )
 }
 
 class PortfolioEditItemComponent extends AbstractFormModelContainer {
-    static defaultProps = {
-        ARCConfig: config
-    }
+  static defaultProps = {
+    ARCConfig: config,
+  }
 
-    onSave = () => {
-        // successful
-        const {id} = this.getModel()
-        navigateTo('/view/' + (id || ''))
-    }
+  onSave = () => {
+    // successful
+    const { id } = this.getModel()
+    navigateTo("/view/" + (id || ""))
+  }
 
-    render() {
+  render() {
+    if (!this.isLoaded()) return <Loader />
 
-        if (!this.isLoaded()) return (<Loader />)
+    const model = this.getModel()
+    const isNew = this.isNew()
 
-        const model = this.getModel()
-        const isNew = this.isNew()
+    //const url = `${process.env.PUBLIC_URL}/images/image-${model.id}.jpg`
 
-        return (
-            <div>
-                <Toolbar>
-                    <div className="row">
-                        <div className="col-sm-6 col-md-6">
-                            <Link to={isNew ? '/' : '/view/' + model.id}>
-                                <button className="btn btn-default">back</button>
-                            </Link>
-                        </div>
-                    </div>
-                </Toolbar>
-                {this.isSyncing() ? <Toast>syncing...</Toast> : null}
-                <div className="polaroid detailed sizing animated fadeIn">
-                    <div className="card-title">
-                        <h3>Edit {model.title}</h3>
-                    </div>
-                    {this.getError() ? <Error>...mmm, something wrong happened...</Error> : null }
-                    <form className="form-horizontal">
-                        <FormRow name="title">
-                            <input className="form-control" name="title" value={model.title}
-                                   onChange={(e) => this.changeProp('title', e.target.value)}/>
-                        </FormRow>
-                        <FormRow name="description">
-                        <textarea rows="10" className="form-control" name="description" value={model.description}
-                              onChange={(e) => this.changeProp('description', e.target.value)}/>
-                        </FormRow>
-                        <FormRow>
-                            <div className="text-right">
-                                <Link to={'/view/' + model.id}>
-                                    <button type="submit" className="btn btn-default">Cancel</button>
-                                </Link>
-                                <button type="button" onClick={this.save} style={{marginLeft: '10px'}}
-                                        className="btn btn-primary">Save
-                                </button>
-                            </div>
-                        </FormRow>
-                    </form>
-                </div>
-            </div>)
-    }
+    return (
+      <div>
+        <Toolbar>
+          <div className="row">
+            <div className="col-sm-6 col-md-6">
+              <Link to={isNew ? "/" : "/view/" + model.id}>
+                <button className="btn btn-default">back</button>
+              </Link>
+            </div>
+          </div>
+        </Toolbar>
+        {this.isSyncing() ? <Toast>syncing...</Toast> : null}
+        <div className="polaroid detailed sizing animated fadeIn">
+          <div className="card-title">
+            <h3>Edit {model.title}</h3>
+          </div>
+          {this.getError() ? (
+            <Error>...mmm, something wrong happened...</Error>
+          ) : null}
+          <form className="form-horizontal">
+            <FormRow name="title">
+              <input
+                className="form-control"
+                name="title"
+                value={model.title}
+                onChange={(e) => this.changeProp("title", e.target.value)}
+              />
+            </FormRow>
+            <FormRow name="description">
+              <textarea
+                rows="10"
+                className="form-control"
+                name="description"
+                value={model.description}
+                onChange={(e) => this.changeProp("description", e.target.value)}
+              />
+            </FormRow>
+            <FormRow>
+              <div className="text-right">
+                <Link to={"/view/" + model.id}>
+                  <button type="submit" className="btn btn-default">
+                    Cancel
+                  </button>
+                </Link>
+                <button
+                  type="button"
+                  onClick={this.save}
+                  style={{ marginLeft: "10px" }}
+                  className="btn btn-primary"
+                >
+                  Save
+                </button>
+              </div>
+            </FormRow>
+          </form>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default mixerConnector(connect, config)(PortfolioEditItemComponent)
--- demo/src/portfolio/components/PorfolioItemComponent.js
+++ demo/src/portfolio/components/PorfolioItemComponent.js
@@ -1,56 +1,64 @@
-import React  from 'react'
-import config from '../config.json'
-import {connect} from 'react-redux'
+import React from "react"
+import config from "../config.json"
+import { connect } from "react-redux"
 
-import {AbstractModelContainer, mixerConnector} from '../../lib'
-import {Loader} from '../../layout/components/loader'
-import {Toolbar} from '../../layout/components/toolbar'
-import {Toast} from '../../layout/components/toast'
-import {LargeError} from '../../layout/components/error'
-import Link from '../../navigation/Link'
+import { AbstractModelContainer, mixerConnector } from "../../lib"
+import { Loader } from "../../layout/components/loader"
+import { Toolbar } from "../../layout/components/toolbar"
+import { Toast } from "../../layout/components/toast"
+import { LargeError } from "../../layout/components/error"
+import Link from "../../navigation/Link"
 
 class PortfolioItemComponent extends AbstractModelContainer {
-    static defaultProps = {
-        ARCConfig: config
+  static defaultProps = {
+    ARCConfig: config,
+  }
+
+  render() {
+    const error = this.getError()
+    if (error) {
+      return (
+        <LargeError
+          title={error?.response?.status || "Error"}
+          children={"...mmm, something wrong happened..."}
+        />
+      )
     }
+    if (!this.isLoaded()) return <Loader />
 
-    render() {
-        const error = this.getError()
-        if (error) {
-            return (<LargeError title={error.response.status} children={'...mmm, something wrong happened...'}/>)
-        }
-        if (!this.isLoaded()) return (<Loader />)
+    const model = this.getModel()
 
-        const model = this.getModel()
+    const url = `${process.env.PUBLIC_URL}/images/image-${model.id}.jpg`
 
-        return ( <div>
-            {this.isSyncing() ? <Toast>syncing...</Toast> : null}
-            <Toolbar>
-                <div className="row">
-                    <div className="col-sm-6 col-md-6">
-                        <Link to={'/'}>
-                            <button className="btn btn-default">back</button>
-                        </Link>
-                    </div>
-                    <div className="col-sm-6 col-md-6 text-right">
-                        <Link to={'/edit/' + model.id + ''}>
-                            <button className="btn btn-primary pull-right">edit</button>
-                        </Link>
-                    </div>
-                </div>
-            </Toolbar>
-            <div className="polaroid detailed sizing animated fadeIn">
-                <Link to={'/edit/' + model.id}>
-                    <img src={/*item.thumbnailUrl*/ 'images/image-' + model.id + '.jpg'}
-                         alt={model.title}/>
-                </Link>
-                <div className="caption">
-                    <h3>{model.title}</h3>
-                    <p>{model.description}</p>
-                </div>
+    return (
+      <div>
+        {this.isSyncing() ? <Toast>syncing...</Toast> : null}
+        <Toolbar>
+          <div className="row">
+            <div className="col-sm-6 col-md-6">
+              <Link to={"/"}>
+                <button className="btn btn-default">back</button>
+              </Link>
             </div>
-        </div>)
-    }
+            <div className="col-sm-6 col-md-6 text-right">
+              <Link to={"/edit/" + model.id + ""}>
+                <button className="btn btn-primary pull-right">edit</button>
+              </Link>
+            </div>
+          </div>
+        </Toolbar>
+        <div className="polaroid detailed sizing animated fadeIn">
+          <Link to={"/edit/" + model.id}>
+            <img src={url} alt={model.title} />
+          </Link>
+          <div className="caption">
+            <h3>{model.title}</h3>
+            <p>{model.description}</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default mixerConnector(connect, config)(PortfolioItemComponent)
