bccdcc818310feff0f41edb6ea2b0e67044bcd24
Alaa El Helw <alaa_el_helw@carrefour.com>
2025-07-22 16:20:53 +0200

    feat: add useARC hook with CRUD methods and state management

--- lib/cjs/hooks/useARC.d.ts
+++ lib/cjs/hooks/useARC.d.ts
@@ -1,36 +1,7 @@
-import { ARC } from "./arc";
-import { ComponentProps, ComponentPropsWithRequiredModelParams } from "../types/components.types";
+import { ComponentProps } from "../types/components.types";
 import { ARCConfig } from "../types/config.types";
+import { UseARCReturn } from "../types/hooks.types";
 export declare function useARC<Model>({ ARCConfig, props, }: {
     ARCConfig: ARCConfig<Model>;
     props: ComponentProps;
-}): {
-    error: object | null;
-    loading: boolean;
-    loaded: boolean;
-    response: Response | null;
-    arc: {
-        arc: ARC<Model>;
-        get: (args: {
-            props?: ComponentProps | undefined;
-            params: ComponentPropsWithRequiredModelParams;
-        }) => Promise<void | Response> | undefined;
-        remove: (args: {
-            props?: ComponentProps | undefined;
-            params: ComponentPropsWithRequiredModelParams;
-        }) => Promise<void | Response> | undefined;
-        create: (args: {
-            props?: ComponentProps | undefined;
-            params: ComponentPropsWithRequiredModelParams;
-            body: any;
-        }) => Promise<void | Response> | undefined;
-        update: (args: {
-            props?: ComponentProps | undefined;
-            params: ComponentPropsWithRequiredModelParams;
-            body: any;
-        }) => Promise<void | Response> | undefined;
-        extract: (props: ComponentProps) => ComponentPropsWithRequiredModelParams;
-        extractParams: (props: ComponentProps) => ComponentPropsWithRequiredModelParams;
-        custom: (fetcher: () => Promise<Response>) => Promise<void | Response> | undefined;
-    };
-};
+}): UseARCReturn<Model>;
---
+++ src/types/hooks.types.ts
@@ -0,0 +1,83 @@
+import { ARC } from "../hooks/arc"
+import { ComponentProps, ComponentPropsWithRequiredModelParams } from "./components.types"
+
+/**
+ * État interne du hook useARC
+ */
+export interface UseARCState {
+  error: null | object
+  loading: boolean
+  loaded: boolean
+  response: Response | null
+  pending: boolean
+}
+
+/**
+ * Interface pour les méthodes CRUD exposées par le hook useARC
+ */
+export interface UseARCMethods<Model> {
+  /**
+   * Instance ARC utilisée en interne
+   */
+  arc: ARC<Model>
+
+  /**
+   * Récupère une ressource
+   */
+  get: (args: {
+    props?: ComponentProps
+    params: ComponentPropsWithRequiredModelParams
+  }) => Promise<Response> | undefined
+
+  /**
+   * Supprime une ressource
+   */
+  remove: (args: {
+    props?: ComponentProps
+    params: ComponentPropsWithRequiredModelParams
+  }) => Promise<Response> | undefined
+
+  /**
+   * Crée une nouvelle ressource
+   */
+  create: (args: {
+    props?: ComponentProps
+    params: ComponentPropsWithRequiredModelParams
+    body: any
+  }) => Promise<Response> | undefined
+
+  /**
+   * Met à jour une ressource existante
+   */
+  update: (args: {
+    props?: ComponentProps
+    params: ComponentPropsWithRequiredModelParams
+    body: any
+  }) => Promise<Response> | undefined
+
+  /**
+   * Extrait les paramètres requis à partir des props
+   */
+  extract: (props: ComponentProps) => ComponentPropsWithRequiredModelParams
+
+  /**
+   * Alias pour extract
+   */
+  extractParams: (props: ComponentProps) => ComponentPropsWithRequiredModelParams
+
+  /**
+   * Permet d'exécuter une requête personnalisée
+   */
+  custom: (fetcher: () => Promise<Response>) => Promise<Response> | undefined
+}
+
+/**
+ * Type de retour complet du hook useARC
+ */
+export interface UseARCReturn<Model> {
+  error: null | object
+  loading: boolean
+  loaded: boolean
+  response: Response | null
+  arc: UseARCMethods<Model>
+}
