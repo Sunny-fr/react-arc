6f46bbb7f8f870961dee2150b9592a2ea643ef8e
Alaa El Helw <alaa_el_helw@carrefour.com>
2025-08-11 09:40:53 +0200

    feat: add support for selectors in HOC and useARC for enhanced state management

--- lib/cjs/HOC/createHOC.d.ts
+++ lib/cjs/HOC/createHOC.d.ts
@@ -1,9 +1,11 @@
 import React from 'react';
 import { ARCConfig } from "../types/config.types";
 import { ARCContainer, ARCContainerProps, RenderComponent } from "../types/components.types";
+import { SelectorFn } from "../types/connectors.types";
 interface CreateHOCParams<Model, RequiredProps = {}, OwnProps = {}> {
     Container?: ARCContainer<Model, RequiredProps, OwnProps>;
     ARCConfig: ARCConfig<Model, RequiredProps>;
+    selectors?: SelectorFn<OwnProps>[];
 }
-export declare function createHOC<Model, RequiredProps = {}, OwnProps = {}>({ Container, ARCConfig }: CreateHOCParams<Model, RequiredProps, OwnProps>): <OverriddenRequiredProps extends RequiredProps = RequiredProps>(Wrapped: RenderComponent<Model, OverriddenRequiredProps, OwnProps>) => <OwnPropsPassed extends OwnProps = OwnProps>(visibleProps: OwnPropsPassed & OverriddenRequiredProps) => React.ReactElement<ARCContainerProps<Model, OverriddenRequiredProps, OwnProps>>;
+export declare function createHOC<Model, RequiredProps = {}, OwnProps = {}>({ Container, ARCConfig, selectors }: CreateHOCParams<Model, RequiredProps, OwnProps>): <OverriddenRequiredProps extends RequiredProps = RequiredProps>(Wrapped: RenderComponent<Model, OverriddenRequiredProps, OwnProps>) => <OwnPropsPassed extends OwnProps = OwnProps>(visibleProps: OwnPropsPassed & OverriddenRequiredProps) => React.ReactElement<ARCContainerProps<Model, OverriddenRequiredProps, OwnProps>>;
 export {};
--- lib/esm/hooks/useARC.d.ts
+++ lib/esm/hooks/useARC.d.ts
@@ -1,6 +1,8 @@
 import { ARCConfig } from "../types/config.types";
+import { SelectorFn } from "../types/connectors.types";
 import { UseARC } from "../types/hooks.connected.types";
-export declare function useARC<Model, RequiredProps extends object = {}, OwnProps extends object = {}>({ ARCConfig: initialConfig, props, }: {
+export declare function useARC<Model, RequiredProps extends object = {}, OwnProps extends object = {}>({ ARCConfig: initialConfig, props, selectors }: {
     ARCConfig: ARCConfig<Model, RequiredProps>;
     props: RequiredProps & OwnProps;
+    selectors?: SelectorFn<OwnProps>[];
 }): UseARC<Model, RequiredProps>;
